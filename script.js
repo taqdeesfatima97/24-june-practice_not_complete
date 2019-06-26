61c392fc7ae1e8f189645818f66c328524485d3c
taqdeesfatima97 <50422652+taqdeesfatima97@users.noreply.github.com>
2019-06-27 00:03:03 +0500

    js.practice

---
+++ script.js
@@ -0,0 +1,16 @@
+// function count() {
+//     let firstNumber = document.querySelector("#firstNumb").value;
+//     let lastNumber = document.querySelector("#lastNumb").value;
+//     for (let i = firstNumber; i > lastNumber; i--) {
+//         console.log(i - 1);
+//     }
+
+// }
+
+function count() {
+    let firstNumber = document.querySelector("#firstNumb").value;
+    let lastNumber = document.querySelector("#lastNumb").value;
+    for (let i = firstNumber; i > lastNumber; i--) {
+        console.log(i - 1);
+    }
+}
